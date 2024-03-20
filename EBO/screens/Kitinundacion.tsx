8e9db661fa8908deb6543e75e6dd0cdf45a632d9
tasajos <tasajos@gmail.com>
2024-03-19 20:22:20 -0400

    actualizacion kitinundacion

--- EBO/screens/Kitinundacion.tsx
+++ EBO/screens/Kitinundacion.tsx
@@ -1,23 +1,85 @@
-// Kitset.tsx
+// Kitinundacion.tsx
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, FlatList, TouchableOpacity, SafeAreaView,ActivityIndicator  } from 'react-native';
+import { View, Text, StyleSheet, Image, FlatList, TouchableOpacity, SafeAreaView, Alert } from 'react-native';
 import { StackNavigationProp } from '@react-navigation/stack';
-import database from '@react-native-firebase/database';
 import { useNavigation } from '@react-navigation/native';
 import FloatingButtonBar from './FloatingButtonBar';
 
-// Define el tipo para tus kits de emergencia
-type RootStackParamList = {
-    // ... otros parámetros de tus rutas
-    Kitset: undefined; // Asegúrate de tener una ruta Kitset en tu StackNavigator
-  };
-const Kitinundacion = () => {
- 
-  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
-  const [loading, setLoading] = useState(true);
+type KitItem = {
+  name: string;
+  isEssential: boolean;
+  selected?: boolean;
+};
 
-  
- 
+type RootStackParamList = {
+  Kitset: undefined;
+};
+
+const shuffleArray = (array: KitItem[]): KitItem[] => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+};
+
+const Kitinundacion = () => {
+  const navigation = useNavigation<StackNavigationProp<RootStackParamList>>();
+
+  const initialItems: KitItem[] = [
+    { name: 'Bolsas impermeables', isEssential: true },
+    { name: 'Copia de seguridad de documentos importantes', isEssential: true },
+    { name: 'Agua embotellada', isEssential: true },
+    { name: 'Comida no perecedera', isEssential: true },
+    { name: 'Pilas', isEssential: true },
+    { name: 'Radio a pilas', isEssential: true },
+    { name: 'Ropa de cambio', isEssential: true },
+    { name: 'Kit de higiene personal', isEssential: true },
+    { name: 'Dinero', isEssential: false },
+    { name: 'Computadora', isEssential: false },
+  ];
+
+  const [items, setItems] = useState<KitItem[]>(shuffleArray([...initialItems]));
+  const [gameOver, setGameOver] = useState(false);
+  const [points, setPoints] = useState(0);
+  const [messageShown, setMessageShown] = useState(false);
+
+  useEffect(() => {
+    setItems(shuffleArray([...initialItems]));
+  }, []);
+
+  const handleSelectItem = (index: number) => {
+    if (gameOver) {
+      Alert.alert('Juego terminado', 'Ya seleccionaste un elemento no esencial.');
+      return;
+    }
+
+    const newItems = [...items];
+    const currentItem = newItems[index];
+
+    currentItem.selected = !currentItem.selected;
+    if (currentItem.isEssential && currentItem.selected) {
+      const newPoints = points + 2;
+      setPoints(newPoints);
+      if (newPoints >= 6 && !messageShown) {
+        setMessageShown(true);
+        Alert.alert('¡Bien hecho!', 'Has alcanzado los puntos necesarios para ganar, pero el juego continúa hasta que selecciones un elemento no esencial.');
+      }
+    } else if (!currentItem.isEssential && currentItem.selected) {
+      setGameOver(true);
+      Alert.alert('Juego terminado', 'Seleccionaste un elemento no esencial. Puntos finales: ' + points, [{ text: 'Reiniciar Juego', onPress: handleRestartGame }]);
+    }
+
+    setItems(newItems);
+  };
+
+  const handleRestartGame = () => {
+    setItems(shuffleArray([...initialItems]));
+    setGameOver(false);
+    setPoints(0);
+    setMessageShown(false);
+  };
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -25,18 +87,38 @@ const Kitinundacion = () => {
         <Image source={require('../imagenes/tsflo1.png')} style={styles.logo} />
         <Text style={styles.headerText}>Con el Apoyo de Tunari sin Fuego</Text>
       </View>
-      
+      <Text style={styles.description}>
+        Selecciona los elementos que crees que son esenciales para un kit de inundación.
+      </Text>
+      <Text style={styles.points}>Puntos: {points}</Text>
+      <View style={{ flex: 1, paddingBottom: 60 }}>
+        <Image source={require('../imagenes/mochila_inundacion.png')} style={styles.backpackIcon} />
+        <FlatList
+          data={items}
+          keyExtractor={(item, index) => index.toString()}
+          renderItem={({ item, index }) => (
+            <TouchableOpacity
+              style={[
+                styles.kitItem,
+                item.selected === true ? (item.isEssential ? styles.itemCorrect : styles.itemIncorrect) : null,
+              ]}
+              onPress={() => handleSelectItem(index)}
+              disabled={item.selected && !item.isEssential}
+            >
+              <Text style={styles.kitText}>{item.name}</Text>
+            </TouchableOpacity>
+          )}
+        />
+      </View>
       <FloatingButtonBar navigation={navigation} />
     </SafeAreaView>
-    
   );
-  
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff', // Suponiendo que el fondo es blanco
+    backgroundColor: '#fff',
   },
   header: {
     alignItems: 'center',
@@ -44,47 +126,54 @@ const styles = StyleSheet.create({
   },
   headerImage: {
     width: '80%',
-    height: 60, // Altura de tu curva
+    height: 60,
     resizeMode: 'contain',
   },
   headerText: {
     fontSize: 16,
-    //fontWeight: 'bold',
     color: '#424242',
-    marginTop: 20, // Ajustar según tu diseño
+    marginTop: 20,
+  },
+  description: {
+    fontSize: 16,
+    textAlign: 'center',
+    marginHorizontal: 20,
+    marginBottom: 20,
+  },
+  backpackIcon: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+    alignSelf: 'center',
+    margin: 20,
   },
   kitItem: {
-    flex: 1,
-    justifyContent: 'center',
+    padding: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 5,
+    marginBottom: 10,
     alignItems: 'center',
-    margin: 10,
-    height: 150, // Ajustar según tu diseño
   },
-  kitIcon: {
-    width: 80,
-    height: 80,
-    resizeMode: 'contain',
+  itemCorrect: {
+    backgroundColor: 'lightgreen',
+  },
+  itemIncorrect: {
+    backgroundColor: 'salmon',
   },
   kitText: {
-    marginTop: 8,
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
-  row: {
-    flex: 1,
-    justifyContent: 'space-around',
+    fontSize: 18,
   },
   logo: {
-    height: 50, // Ajusta la altura de tu logo
-    resizeMode: 'contain', // Contiene la imagen dentro del espacio disponible sin deformarla
-    marginTop: 20, // Ajusta el margen superior si es necesario
+    height: 50,
+    resizeMode: 'contain',
+    marginTop: 20,
   },
-  spinnerContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+  points: {
+    fontSize: 18,
+    textAlign: 'center',
+    marginVertical: 10,
   },
-  // ... Agrega más estilos según sea necesario
 });
 
 export default Kitinundacion;
