e4a61a49846015342197ab8acbd160b1afcdf19b
cazcarraga <cazcarraga@hipermaxi.com>
2024-03-22 12:14:12 -0400

    update login

--- EBO/screens/Login.tsx
+++ EBO/screens/Login.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, Image,Alert } from 'react-native';
 import FloatingButtonBar from './FloatingButtonBar';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
+import auth from '@react-native-firebase/auth';
 
 // import { TextInput, Button } from 'react-native-paper';
 
@@ -16,14 +17,57 @@ const Login: React.FC<Props> = ({ navigation }) => {
     // ... otros parámetros de tus rutas
     Login: undefined; // Asegúrate de tener una ruta Eventos en tu StackNavigator
   };
-  const handleLogin = () => {
-    // Aquí manejarías el inicio de sesión, por ejemplo, utilizando Firebase Auth
-    // Firebase authentication code here
+  const handleLogin = async () => {
+    // Asegúrate de que el correo electrónico y la contraseña no estén vacíos
+    if (email.trim() === '' || password.trim() === '') {
+      Alert.alert("Error", "Por favor, ingrese un correo electrónico y una contraseña.");
+      return; // Detiene la ejecución si alguno de los campos está vacío
+    }
+  
+    try {
+      let response = await auth().signInWithEmailAndPassword(email, password);
+      if (response && response.user) {
+        Alert.alert("Éxito", "Login Exitoso");
+        navigation.navigate('Dscreen'); // Navega a la pantalla principal
+      }
+    
+    } catch (error: any) { // Agrega el type assertion aquí
+      let message;
+      if (error.code === 'auth/user-not-found') {
+        message = "No existe una cuenta para el correo electrónico ingresado.";
+      } else if (error.code === 'auth/wrong-password') {
+        message = "La contraseña ingresada es incorrecta.";
+      } else {
+        message = "Ha ocurrido un error inesperado. Por favor intente de nuevo.";
+      }
+  
+      Alert.alert("Fallo de inicio de sesión", message);
+    }
   };
 
   const handlePasswordRecovery = () => {
-    // Aquí manejarías la recuperación de contraseña
-    // Password recovery code here
+    // Maneja aquí la recuperación de la contraseña
+    if (email.trim() === '') {
+      Alert.alert('Ingrese email', 'Por favor ingrese su email para resetear su password.');
+      return;
+    }
+    
+    auth().sendPasswordResetEmail(email.trim())
+      .then(() => {
+        Alert.alert('Revisa tu email', 'Link de reinicio de contraseña fue enviada a tu correo');
+      })
+      .catch((error: any) => { // Agrega el type assertion aquí
+        let message = "Ha ocurrido un error inesperado.";
+        if (error.code === 'auth/user-not-found') {
+          message = "No existe una cuenta con ese correo electrónico.";
+        } else if (error.code === 'auth/invalid-email') {
+          message = "El correo electrónico no es válido.";
+        } else {
+          message = error.message; // Puedes optar por mostrar el mensaje de error de Firebase
+        }
+      
+        Alert.alert("Error", message);
+      });
   };
 
   return (
