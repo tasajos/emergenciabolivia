bd3d6f4ee265a6c559eb51ff0f241b73127f8d3e
tasajos <tasajos@gmail.com>
2024-04-03 00:04:01 -0400

    update homv2

--- EBO/screens/Homev2.tsx
+++ EBO/screens/Homev2.tsx
@@ -3,6 +3,8 @@ import { View, Text, StyleSheet, ScrollView, Image, ActivityIndicator } from 're
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import FloatingButtonBar from './FloatingButtonBar';
+import DeviceInfo from 'react-native-device-info';
+import database from '@react-native-firebase/database';
 
 type RootStackParamList = {
   Homev2: undefined;
@@ -15,14 +17,26 @@ type Props = {
 
 const Homev2: React.FC<Props> = ({ navigation }) => {
   const [loading, setLoading] = useState(true);
+  const [appVersion, setAppVersion] = useState('');
+  const [versionName, setVersionName] = useState('');
 
   useEffect(() => {
+
+    setAppVersion(DeviceInfo.getReadableVersion());
+    const getVersionName = async () => {
+      const version = await DeviceInfo.getVersion();
+      setVersionName(version);
+    };
+    getVersionName();  
     // Simula la carga de datos con un retraso de 2 segundos
     setTimeout(() => {
       setLoading(false); // Actualiza el estado para detener la carga
     }, 2000);
   }, []);
 
+
+
+  
   return (
     <View style={styles.container}>
       {loading && (
