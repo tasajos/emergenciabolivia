12c422cda162dfe23f1e521479bc447ce3dbc1cd
tasajos <tasajos@gmail.com>
2024-08-11 10:48:12 -0400

    update notificaciones

--- EBO/index.js
+++ EBO/index.js
@@ -1,19 +1,12 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
-//import firebase from '@react-native-firebase/app';
-//import { firebase } from '@react-native-firebase/app';
+import React, { useState, useEffect } from 'react';
+import { AppRegistry } from 'react-native';
 import { initializeApp } from '@react-native-firebase/app';
 import App from './App';
-import {name as appName} from './app.json';
-//import bgMessaging from './bgMessaging'; // Importa el manejador de mensajes en segundo plano
-import { initializeFCM,requestUserPermissionForNotifications } from './FCMService';
+import { name as appName } from './app.json';
+import { initializeFCM } from './FCMService';
+import NotificationPermissionModal from './screens/NotificationPermissionModal'; // Importa el modal
 
-// Initialize Firebase
 const firebaseConfig = {
-  // Tu configuración de Firebase va aquí
   apiKey: "AIzaSyBOjSCE0nK8peMFZGQ5cLXRgKfBe_41dMk",
   authDomain: "chakuy.com",
   databaseURL: "https://fir-login2-c7a59-default-rtdb.firebaseio.com/",
@@ -23,16 +16,32 @@ const firebaseConfig = {
   appId: "1:306570664024:android:3b7152b52a1dbc691a522d"
 };
 
-//if (!firebase.apps.length) {
-  //firebase.initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
-  initializeApp(firebaseConfig);
-//}
+const Main = () => {
+  const [isModalVisible, setModalVisible] = useState(false);
 
-// Inicializa FCM
-initializeFCM();
-//requestNotificationPermission();
-requestUserPermissionForNotifications();
+  useEffect(() => {
+    // Inicializa FCM
+    initializeFCM();
 
-AppRegistry.registerComponent(appName, () => App);
-//AppRegistry.registerHeadlessTask('RNFirebaseBackgroundMessage', () => bgMessaging); // Registra el manejador de mensajes en segundo plano
+    // Mostrar el modal para solicitar permisos
+    setModalVisible(true);
+  }, []);
+
+  const handleModalClose = () => {
+    setModalVisible(false);
+  };
+
+  return (
+    <>
+      <App />
+      <NotificationPermissionModal
+        isVisible={isModalVisible}
+        onClose={handleModalClose}
+      />
+    </>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => Main);
---
+++ EBO/screens/NotificationPermissionModal.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Linking } from 'react-native';
+import Modal from 'react-native-modal';
+import Icon from 'react-native-vector-icons/Ionicons';
+import messaging from '@react-native-firebase/messaging';
+
+const NotificationPermissionModal = ({ isVisible, onClose }) => {
+  const openAppNotificationSettings = () => {
+    Linking.openSettings(); // Redirigir a la configuración de la app en Android
+  };
+
+  const requestPermission = async () => {
+    const permission = await messaging().requestPermission();
+    onClose(); // Cierra el modal después de solicitar el permiso.
+
+    if (
+      permission === messaging.AuthorizationStatus.AUTHORIZED ||
+      permission === messaging.AuthorizationStatus.PROVISIONAL
+    ) {
+      console.log('Permiso de notificación concedido.');
+      openAppNotificationSettings(); // Redirigir a la configuración de la aplicación.
+    } else {
+      console.log('Permiso de notificación denegado.');
+      openAppNotificationSettings(); // Redirigir a la configuración de la aplicación.
+    }
+  };
+
+  return (
+    <Modal isVisible={isVisible} onBackdropPress={onClose} style={styles.modal}>
+      <View style={styles.modalContent}>
+        <Icon name="notifications" size={50} color="#FF9500" />
+        <Text style={styles.title}>Habilitar Notificaciones</Text>
+        <Text style={styles.message}>
+          ¿Deseas habilitar las notificaciones para recibir las últimas alertas y actualizaciones?
+        </Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.cancelButton} onPress={onClose}>
+            <Text style={styles.buttonText}>No</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.confirmButton} onPress={requestPermission}>
+            <Text style={styles.buttonText}>Sí</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  modal: {
+    justifyContent: 'center',
+    margin: 20,
+  },
+  modalContent: {
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: 'black',
+    marginTop: 10,
+  },
+  message: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: 'black',
+    marginVertical: 15,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  cancelButton: {
+    flex: 1,
+    backgroundColor: '#d9534f',
+    padding: 10,
+    borderRadius: 5,
+    marginRight: 10,
+  },
+  confirmButton: {
+    flex: 1,
+    backgroundColor: '#5cb85c',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+});
+
+export default NotificationPermissionModal;
