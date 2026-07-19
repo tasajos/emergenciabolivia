974927fb2e67db9d8df125771636ee36b89faa0b
Carlos Azcarraga <tasajos@gmail.com>
2026-07-19 13:56:28 -0400

    update_2

--- EBO/react-native.config.js
+++ EBO/react-native.config.js
@@ -1,37 +1,7 @@
-﻿module.exports = {
+module.exports = {
   project: {
     android: {
       sourceDir: './android',
     },
-  },
-  dependencies: {
-    '@react-native-firebase/app': {
-      platforms: {
-        android: null,
-      },
-    },
-    '@react-native-firebase/auth': {
-      platforms: {
-        android: null,
-      },
-    },
-    '@react-native-firebase/storage': {
-      platforms: {
-        android: null,
-      },
-    },
-    '@react-native-firebase/messaging': {
-      platforms: {
-        android: null,
-      },
-    },
-
-     'react-native-gesture-handler': {
-      platforms: {
-        android: {
-          packageImportPath: 'import com.swmansion.gesturehandler.RNGestureHandlerPackage;',
-        },
-         },
-          },
   },
 };
